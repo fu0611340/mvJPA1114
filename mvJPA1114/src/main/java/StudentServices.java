9d49c091ca473a7a091a89e015f6c634eafa0632
student <student@DESKTOP-4ARRLB2>
2022-11-15 13:48:04 +0800

    add Student Restful Services

--- mvJPA1114/src/main/java/StudentServices.java
+++ mvJPA1114/src/main/java/StudentServices.java
@@ -1,6 +1,9 @@
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,16 +15,56 @@ import model.StudentDAO;
 public class StudentServices {
 	
 	@GET
-	public void show() {
-		System.out.println("test");
-	}
-	
-	@GET
 	@Path("/getAll")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Student> getAll(){
 		List<Student> result = null;
 		result = new StudentDAO().getAll();
 		return result;
+	}
+	
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String add(Student s) {
+		try {
+			StudentDAO.add(s);
+		} catch(Exception e){
+			System.out.println("Add Student Error: "+e.getMessage());
+			return "Failed";
+		}
+		return "OK";
+	}
+	
+	@POST
+	@Path("/delete")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String delete(@FormParam("sid") int id) {
+		StudentDAO.delete(id);
+		return "ok";
+	}
+	
+	@POST
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String update(@FormParam("sid") int sid, @FormParam("sname") String sname, @FormParam("age") int age) {
+		Student s = new Student(sid, sname, age);
+		StudentDAO.update(s);
+		return "ok";
+	}
+	
+	@POST
+	@Path("/test")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String addTest(@FormParam("sid") String sidS, @FormParam("sname") String sname, @FormParam("age") String ageS) {
+		int sid = Integer.parseInt(sidS);
+		int age = Integer.parseInt(ageS);
+		Student s = new Student(sid, sname, age);
+		StudentDAO.add(s);
+		return "ok";
 	}
 }
--- mvJPA1114/src/main/java/model/StudentDAO.java
+++ mvJPA1114/src/main/java/model/StudentDAO.java
@@ -8,6 +8,7 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 public class StudentDAO {
+	
 	@SuppressWarnings("unchecked")
 	public static List<Student> getAll(){
 		List<Student> result = null;
@@ -22,5 +23,44 @@ public class StudentDAO {
 		emf.close();
 		
 		return result;
+	}
+	
+	public static void add(Student s) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("mvJPA1114");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(s);
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+	}
+	
+	public static void delete(int id) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("mvJPA1114");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Student s = em.find(Student.class, id);
+		em.remove(s);
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+	}
+	
+	public static void update(Student s) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("mvJPA1114");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Student newS = em.find(Student.class, s.getSid());
+		if(newS != null) {
+			newS.setSname(s.getSname());
+			newS.setAge(s.getAge());
+			em.merge(newS);
+			em.getTransaction().commit();
+		}
+		else {
+			em.getTransaction().rollback();
+		}
+		em.close();
+		emf.close();
 	}
 }
